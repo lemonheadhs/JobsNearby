04a5523c844a33b36d071b41e7c70ed53e837434
lemonheadhs <744640996@qq.com>
2019-01-03 18:18:41 +0800

    update

---
+++ src/JobsNearby.Api/Types.fs
@@ -0,0 +1,32 @@
+﻿module JobsNearby.Types
+
+open FSharp.Azure.StorageTypeProvider
+open FSharp.Data
+
+type Azure = AzureTypeProvider<configFileName = "web.config", connectionStringName = "azureStorage">
+
+type JobsResults = FSharp.Data.JsonProvider<"../../samples/jobs.json">
+
+type RouteInfo = JsonProvider<"../../samples/routeSearchResp.json">
+
+
+[<CLIMutable>]
+type JobDataDto =
+    { category: string
+      color: string
+      companyName: string
+      distance: string
+      link: string
+      markerRadius: int
+      name: string
+      salaryEstimate: float
+      scale: string
+    }
+[<CLIMutable>]
+type CompanyDto =
+    { DetailUrl: string
+      Latitude: float
+      Longitude: float
+      Name: string
+      Distances: string
+    }
