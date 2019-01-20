e5ab36ced7200016d76269c3fa200db2e1934252
lemonheadhs <744640996@qq.com>
2019-01-20 23:23:57 +0800

    update build script

--- build.fsx
+++ build.fsx
@@ -3,7 +3,7 @@ nuget Fake.Core.Target
 nuget Fake.IO.FileSystem
 nuget Fake.IO.Zip
 nuget Fake.Azure.Kudu
-nuget Fake.DotNet.MSBuild
+nuget Fake.DotNet.Cli
 nuget FSharp.Data"
 #load "./.fake/build.fsx/intellisense.fsx"
 
@@ -19,58 +19,51 @@ System.Environment.CurrentDirectory = __SOURCE_DIRECTORY__
 printfn __SOURCE_DIRECTORY__
 
 let apiProject = "./src/JobsNearby.Api/JobsNearby.Api.fsproj"
-
 let zipPackageDir = "./zipTemp" |> Path.GetFullPath
 let buildOutputDir = "./src/JobsNearby.Api/bin/Release" |> Path.GetFullPath
 
-Target.create "Clean" (fun _ ->
-    Shell.cleanDirs [ buildOutputDir; zipPackageDir ]
-)
+Target.create "Clean"
+    (fun _ -> Shell.cleanDirs [ buildOutputDir; zipPackageDir ])
 
 Target.create "Build" (fun _ ->
-    let setParams (defaults:MSBuildParams) =
-        { defaults with
-            Verbosity = Some(Quiet)
-            Targets = ["Build"]
-            Properties =
-                [
-                    "Optimize", "True"
-                    "DebugSymbols", "True"
-                    "Configuration", "Release"
-                ]
-         }
-    MSBuild.build setParams apiProject
-)
+    let setParams (defaults : DotNet.PublishOptions) =
+        { defaults with Configuration = DotNet.BuildConfiguration.Release
+                        Framework = Some "netcoreapp2.2"
+                        OutputPath = Some buildOutputDir }
+    DotNet.publish setParams apiProject)
+    
+Target.create "Zip"
+    (fun _ ->
+    !!Path.Combine(buildOutputDir, "**/*")
+    |> Zip.createZip buildOutputDir (Path.Combine(zipPackageDir, "publish.zip"))
+           "" Zip.DefaultZipLevel false)
 
-Target.create "Zip" (fun _ ->
-    !! Path.Combine(buildOutputDir, "**/*")
-    |> Zip.createZip buildOutputDir (Path.Combine(zipPackageDir, "publish.zip")) "" Zip.DefaultZipLevel false
-)
+type PubProfile = JsonProvider<"./samples/PubProfileSample.json", ResolutionFolder=__SOURCE_DIRECTORY__>
 
-type PubProfile = JsonProvider<"./samples/PubProfileSample.json", ResolutionFolder = __SOURCE_DIRECTORY__>
-let pubSettings = PubProfile.Load(Path.combine __SOURCE_DIRECTORY__ "samples/PubProfiles.json")
+let pubSettings =
+    PubProfile.Load(Path.combine __SOURCE_DIRECTORY__ "samples/PubProfiles.json")
 
 Target.create "Deploy" (fun _ ->
     let zipPath = Path.Combine(zipPackageDir, "publish.zip")
     pubSettings
-    |> Seq.iter(fun p ->
-                    let deployParams: Kudu.ZipDeployParams = 
-                        { PackageLocation = zipPath
-                          Password = p.GitPassword
-                          Url = p.GitUrl |> (sprintf "https://%s") |> System.Uri
-                          UserName = p.GitUsername }
-                    Kudu.zipDeploy deployParams
-                    printfn "App Service %s deployed" p.Name)
-    printfn "All App Services are updated!"
-)
+    |> Seq.iter (fun p ->
+           let deployParams : Kudu.ZipDeployParams =
+               { PackageLocation = zipPath
+                 Password = p.GitPassword
+                 Url =
+                     p.GitUrl
+                     |> (sprintf "https://%s")
+                     |> System.Uri
+                 UserName = p.GitUsername }
+           Kudu.zipDeploy deployParams
+           printfn "App Service %s deployed" p.Name)
+    printfn "All App Services are updated!")
 
 open Fake.Core.TargetOperators
 
-"Clean"
-    ==> "Build"
-    ==> "Zip"
+"Clean" 
+    ==> "Build" 
+    ==> "Zip" 
     ==> "Deploy"
 
-
 Target.runOrDefault "Deploy"
-
